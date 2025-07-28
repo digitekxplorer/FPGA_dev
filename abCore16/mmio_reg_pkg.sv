f1fcd1478dc86d30684936697452cbf2af4c1daf
Al Baeza <164692754+digitekxplorer@users.noreply.github.com>
2025-07-28 11:24:12 -0600

    Update mmio_reg_pkg.sv
    Uart Fifos

--- abCore16/mmio_reg_pkg.sv
+++ abCore16/mmio_reg_pkg.sv
@@ -35,11 +35,11 @@ package mmio_reg_pkg;
   
   // UART Status Register (0x12)
   typedef struct packed {
-    logic [12:0] reserved;      // Reserved bits [15:3]
-    logic        rx_error;      // RX frame error [2]
-    logic        rx_valid;      // RX data valid [1]
-//    logic        tx_busy;       // TX busy status [0]
-    logic        tx_fifo_avail; // TX FIFO status [0]
+    logic [11:0] reserved;           // Reserved bits [15:4]
+    logic        rx_fifo_prog_full;  // RX FIFO programed full [3]
+    logic        rx_error;           // RX frame error [2]
+    logic        rx_data_avail;      // RX Fifo data valid [1]
+    logic        tx_fifo_avail;      // TX FIFO status [0]
   } uart_status_t;
   
   // UART Data Registers (0x14, 0x16)
