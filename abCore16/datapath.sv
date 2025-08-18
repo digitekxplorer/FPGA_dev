5db1e488b9b32f0664e9feb3eb0b25f6c54c67d5
Al Baeza <164692754+digitekxplorer@users.noreply.github.com>
2025-08-18 07:43:27 -0600

    Update datapath.sv
    Added Interrupts

--- abCore16/datapath.sv
+++ abCore16/datapath.sv
@@ -28,7 +28,9 @@ module datapath (
     input  logic clk,
     input  logic rst_n,
 
-    // To Instruction Memory
+    // Instruction Memory
+    input  logic [7:0]             imem_rdata_i,     // interrupt vector address
+    input  logic                   grant_vec_i,      // granted interrupt number
     output logic [`ADDR_WIDTH-1:0] pc_to_imem_addr,
 
     // Control Signals from Control Unit
@@ -40,6 +42,7 @@ module datapath (
     input  logic [`REG_ADDR_WIDTH-1:0] rf_src2_addr_from_cu,
     input  logic [1:0]  rf_write_data_sel_from_cu,
     input  logic        flags_write_en_from_cu,
+//    input  logic        reti_flags_sel_from_cu,
     input  logic [`DATA_WIDTH-1:0] imm_val_from_cu, // Assembled immediate from CU
     input  logic [3:0]  alu_op_from_cu,
     input  logic        alu_src_a_sel_from_cu,
@@ -47,8 +50,10 @@ module datapath (
     input  logic        dmem_write_en_from_cu,
     input  logic [1:0]  dmem_addr_sel_from_cu,
     input  logic [1:0]  dmem_data_sel_from_cu,
+    input  logic        imem_addr_sel_from_cu,
     input  logic        sp_op_inc_from_cu,
     input  logic        sp_op_dec_from_cu,
+    input  logic        save_pc_from_cu,
 
     // Data Memory Interface
     output logic                   dmem_we_out,
@@ -75,23 +80,124 @@ logic [`DATA_WIDTH-1:0] alu_result_internal;
 logic alu_zf, alu_sf, alu_cf, alu_of;
 // PC
 logic [`ADDR_WIDTH-1:0] pc_reg, pc_next_calculated;
+logic [`ADDR_WIDTH-1:0] pc_int_reti;
+logic [ 3:0]            flags_sav;
 // Register File
 logic [`DATA_WIDTH-1:0] rf_rdata1, rf_rdata2;
 logic [`DATA_WIDTH-1:0] rf_wdata_final;
 logic [`NUM_GP_REGS-1:0] [`DATA_WIDTH-1:0] register_file;
 logic [`ADDR_WIDTH-1:0] sp_reg;
 
-    //================================================================
-    // Program Counter (PC)
-    //================================================================
-    assign pc_to_imem_addr = pc_reg;
+logic [`ADDR_WIDTH-1:0] imem_addr_gen;
+logic [15:0]            grant_int_addr;
+logic [ 3:0]            irq_vec_sav;
+logic                   imem_addr_sel_from_cu_r;
+logic                   imem_addr_sel_fedge;
+logic                   imem_addr_sel_redge;
 
+    //================================================================
+    // Program Counter (PC) and Flags
+    //================================================================
+    // Insruction memory address mux to read from IVT for interrupt entry
+    always_comb begin
+        if (imem_addr_sel_from_cu) begin
+            pc_to_imem_addr = imem_addr_gen;  // IVT_BASE + (irq_num << 1) + 1
+        end
+        else begin
+            pc_to_imem_addr = pc_reg;
+        end
+    end
+    
+    // imem_addr generator for interrupts
+    // IVT_BASE + (irq_num << 1) + 1
+    always_ff @(posedge clk or negedge rst_n) begin
+        if (!rst_n) begin; 
+            imem_addr_gen   <= `IVT_BASE_ADDR;  // IVT_BASE
+        end
+        // get interrupt address
+        else if (imem_addr_sel_redge) begin
+            imem_addr_gen   <= `IVT_BASE_ADDR + irq_vec_sav;  // IVT_BASE + (irq_num << 1)
+        end         
+        else if (imem_addr_sel_from_cu) begin
+            imem_addr_gen   <= imem_addr_gen + 1;   // IVT_BASE + (irq_num << 1) + 1
+        end
+        // set for next interrput
+        else if (imem_addr_sel_fedge) begin
+            imem_addr_gen   <= `IVT_BASE_ADDR;  // IVT_BASE + (irq_num << 1)
+        end   
+    end
+    
+    // Capture interrupt number in service
+    always_ff @(posedge clk or negedge rst_n) begin
+        if (!rst_n) begin; 
+            irq_vec_sav   <= 3'h0;  // interrupt number
+        end
+        // save the interrupt number when irq is granted
+        else if (pic_bus.intrpt) begin
+            irq_vec_sav   <=  grant_vec_i;  // IVT_BASE + (irq_num << 1)
+        end
+        // get ready for next interrupt
+        else if (imem_addr_sel_fedge) begin
+            irq_vec_sav   <=  '0;  
+        end         
+    end
+    
+    // one clock delay
+    logic [2:0] imem_addr_sel_shft;
+    always_ff @(posedge clk or negedge rst_n) begin
+        if (!rst_n) begin
+            imem_addr_sel_from_cu_r <= 1'b0;
+            imem_addr_sel_shft      <= 3'b000;
+            imem_addr_sel_fedge     <= 1'b0; 
+        end
+        else begin
+            imem_addr_sel_from_cu_r <= imem_addr_sel_from_cu;
+            imem_addr_sel_shft <= { imem_addr_sel_shft[1:0], imem_addr_sel_from_cu };
+            if (imem_addr_sel_shft[1:0] == 2'b01 ) begin
+               imem_addr_sel_fedge  <= 1'b1;
+            end 
+            else begin
+               imem_addr_sel_fedge  <= 1'b0;
+            end 
+        end 
+    end
+    assign imem_addr_sel_redge = (imem_addr_sel_shft[1:0] == 2'b10);
+    
+    
+    always_ff @(posedge clk or negedge rst_n) begin
+        if (!rst_n) begin
+            grant_int_addr      <= 16'h0; 
+        end
+        else if (imem_addr_sel_from_cu_r) begin
+            if (imem_addr_gen[0]) begin
+                grant_int_addr[7:0]      <= imem_rdata_i; 
+            end
+            else begin
+                grant_int_addr[15:8]     <= imem_rdata_i; 
+            end 
+        end 
+    end
+    
+//    assign pc_to_imem_addr = pc_reg;
+
+    // save_pc_from_cu
     always_ff @(posedge clk or negedge rst_n) begin
         if (!rst_n) 
             pc_reg <= `ADDR_WIDTH'h0000;
         else if (pc_write_en_from_cu) 
             pc_reg <= pc_next_calculated;
     end
+    
+    // save PC during Interrupt process
+    always_ff @(posedge clk or negedge rst_n) begin
+        if (!rst_n) begin
+            pc_int_reti <= '0;
+        end
+        else if (save_pc_from_cu) begin
+            pc_int_reti <= pc_reg;
+        end
+    end
+
     
     // PC Next Logic
     always_comb begin
@@ -101,10 +207,67 @@ logic [`ADDR_WIDTH-1:0] sp_reg;
             `PC_SRC_MEM:        pc_next_calculated = dmem_rdata_in;        //010
             `PC_SRC_PC_CURRENT: pc_next_calculated = pc_reg;               //011
             `PC_SRC_ALU:        pc_next_calculated = alu_result_internal;  //100
+            // Entry address for Interrupt from IVT in instruction memory
+            `PC_SRC_IVT:        pc_next_calculated = grant_int_addr;       //101
+            // RETI after interrupt; restore PC
+            `PC_SRC_RESTORE:    pc_next_calculated = pc_int_reti;          //110
             default:            pc_next_calculated = pc_reg;
         endcase
     end
 
+    // --- Flags Logic ---
+    // save  flags during Interrupt process
+    always_ff @(posedge clk or negedge rst_n) begin
+        if (!rst_n) begin
+            flags_sav  <= 4'h0;
+        end
+        else if (save_pc_from_cu) begin
+            flags_sav  <= {ZF_to_cu, SF_to_cu, CF_to_cu, OF_to_cu};
+        end
+    end
+    
+    
+    always_ff @(posedge clk or negedge rst_n) begin
+        if (!rst_n) begin
+            ZF_to_cu <= 1'b0;
+            SF_to_cu <= 1'b0;
+            CF_to_cu <= 1'b0;
+            OF_to_cu <= 1'b0;
+        end
+        else if (flags_write_en_from_cu) begin 
+            // restore flags save during interrupt entry
+//            if (reti_flags_sel_from_cu) begin
+            if (pc_src_sel_from_cu == `PC_SRC_RESTORE) begin
+                ZF_to_cu <= flags_sav[3];
+                SF_to_cu <= flags_sav[2];
+                CF_to_cu <= flags_sav[1];
+                OF_to_cu <= flags_sav[0];
+            end
+            // normal flags saved
+            else begin
+                ZF_to_cu <= alu_zf;
+                SF_to_cu <= alu_sf;
+                CF_to_cu <= alu_cf;
+                OF_to_cu <= alu_of;            
+            end
+        end
+    end
+ 
+
+//    always_ff @(posedge clk or negedge rst_n) begin
+//        if (!rst_n) begin
+//            ZF_to_cu <= 1'b0;
+//            SF_to_cu <= 1'b0;
+//            CF_to_cu <= 1'b0;
+//            OF_to_cu <= 1'b0;
+//        end
+//        else if (flags_write_en_from_cu) begin 
+//            ZF_to_cu <= alu_zf;
+//            SF_to_cu <= alu_sf;
+//            CF_to_cu <= alu_cf;
+//            OF_to_cu <= alu_of;
+//        end
+//    end
 
     //================================================================
     // Register File (R0-R7) and Stack Pointer (SP)
@@ -139,23 +302,6 @@ logic [`ADDR_WIDTH-1:0] sp_reg;
         else if (rf_write_en_from_cu && rf_dest_addr_from_cu == `SP_REG_ADDR) // For MOVTOSP
             sp_reg <= rf_wdata_final;
     end
-    
-    // Flags Logic
-    always_ff @(posedge clk or negedge rst_n) begin
-        if (!rst_n) begin
-            ZF_to_cu <= 1'b0;
-            SF_to_cu <= 1'b0;
-            CF_to_cu <= 1'b0;
-            OF_to_cu <= 1'b0;
-        end
-        else if (flags_write_en_from_cu) begin 
-            ZF_to_cu <= alu_zf;
-            SF_to_cu <= alu_sf;
-            CF_to_cu <= alu_cf;
-            OF_to_cu <= alu_of;
-        end
-    end
- 
 
     //================================================================
     // ALU
@@ -208,6 +354,8 @@ logic [`ADDR_WIDTH-1:0] sp_reg;
             `DMEM_ADDR_SRC_IMM: dmem_addr_out = imm_val_from_cu;   // 00
             `DMEM_ADDR_SRC_SP:  dmem_addr_out = sp_reg;            // 01
             `DMEM_ADDR_SRC_ALU: dmem_addr_out = alu_result_internal; // For LOADI/STORI
+            // IVT entry address
+            `DMEM_ADDR_SRC_IVT: dmem_addr_out = `IVT_BASE_ADDR;    // IVT base + irq_num << 1         
             default:            dmem_addr_out = 16'hDEAD;
         endcase
     end
