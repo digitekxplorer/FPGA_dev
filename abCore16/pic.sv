56cf38743f97c0e610f1d8313e6558134a41c582
Al Baeza <164692754+digitekxplorer@users.noreply.github.com>
2025-08-18 21:25:37 -0600

    Update pic.sv
    Added FSM to PIC.

--- abCore16/pic.sv
+++ abCore16/pic.sv
@@ -44,13 +44,50 @@ module pic (
     logic [15:0] requests_to_service;
     logic        grant_irq;
     logic [3:0]  grant_vec;
+//    logic [3:0]  grant_vec_sav;
+    logic        pending_int;  // OR all irr bits
     
     logic [15:0] devices_irq;
-//    assign devices_irq = pic_bus.irq;
+    logic [15:0] devices_irq_r;
+    logic [15:0] devices_irq_2r;
+    logic [15:0] devices_irq_sav;
+    logic        enable_int_sav;
+    logic        enable_int_sav_clr;
+    logic        new_irq;
+    
+    logic [15:0] isr_sav;
+    logic        grant_irq_gen;
+
+
+    // --- Sequential Logic for Register State ---
+    always_ff @(posedge pic_bus.clk or negedge pic_bus.rst_n) begin
+        if (!pic_bus.rst_n) begin
+            devices_irq_r   <= '0;
+            devices_irq_2r  <= '0;
+            devices_irq_sav <= '0;
+//            grant_vec_sav   <= '0;
+        end 
+        else begin
+            devices_irq_r  <= pic_bus.irq;
+            devices_irq_2r <= devices_irq_r;
+            if (new_irq) begin
+                devices_irq_sav <= devices_irq_2r;
+            end
+            
+            //
+            if (grant_irq) begin
+//                grant_vec_sav <= grant_vec;
+            end 
+       end
+    end
+    
+    assign new_irq = (devices_irq_r != devices_irq_2r);
+    
     
     // Don't allow interrupts until interrupts are enabled
     always_comb begin
-       if (enable_int_i) begin
+//       if (enable_int_i) begin
+       if (enable_int_sav) begin
            devices_irq = pic_bus.irq;
        end
        else begin
@@ -69,15 +106,88 @@ module pic (
         grant_irq = 1'b0;
         grant_vec = 4'h0;
         // Find the first set bit from LSB (IRQ 0) to MSB (IRQ 15)
-        for (int i = 0; i < 16; i++) begin
-            if (enable_int_i) begin           // System level interrupts enabled
-                if (requests_to_service[i]) begin
-                    grant_irq = 1'b1;
-                    grant_vec = i;
-                    break; // Found the highest priority, stop searching
-                end
-            end
+//        for (int i = 0; i < 16; i++) begin
+//            if (enable_int_i) begin           // System level interrupts enabled
+//                if (requests_to_service[i]) begin
+//                    grant_irq = 1'b1;
+//                    grant_vec = i;
+//                    break; // Found the highest priority, stop searching
+//                end
+//            end
+//        end
+        
+//    if (enable_int_sav) begin
+    // grant_irq_gen
+    if (grant_irq_gen) begin
+        // Check from highest priority (0) to lowest priority (15)
+        if (requests_to_service[0]) begin
+            grant_irq = 1'b1;
+            grant_vec = 0;
+        end else if (requests_to_service[1]) begin
+            grant_irq = 1'b1;
+            grant_vec = 1;
+        end else if (requests_to_service[2]) begin
+            grant_irq = 1'b1;
+            grant_vec = 2;
+        end else if (requests_to_service[3]) begin
+            grant_irq = 1'b1;
+            grant_vec = 3;
+        end else if (requests_to_service[4]) begin
+            grant_irq = 1'b1;
+            grant_vec = 4;
+        end else if (requests_to_service[5]) begin
+            grant_irq = 1'b1;
+            grant_vec = 5;
+        end else if (requests_to_service[6]) begin
+            grant_irq = 1'b1;
+            grant_vec = 6;
+        end else if (requests_to_service[7]) begin
+            grant_irq = 1'b1;
+            grant_vec = 7;
+        end else if (requests_to_service[8]) begin
+            grant_irq = 1'b1;
+            grant_vec = 8;
+        end else if (requests_to_service[9]) begin
+            grant_irq = 1'b1;
+            grant_vec = 9;
+        end else if (requests_to_service[10]) begin
+            grant_irq = 1'b1;
+            grant_vec = 10;
+        end else if (requests_to_service[11]) begin
+            grant_irq = 1'b1;
+            grant_vec = 11;
+        end else if (requests_to_service[12]) begin
+            grant_irq = 1'b1;
+            grant_vec = 12;
+        end else if (requests_to_service[13]) begin
+            grant_irq = 1'b1;
+            grant_vec = 13;
+        end else if (requests_to_service[14]) begin
+            grant_irq = 1'b1;
+            grant_vec = 14;
+        end else if (requests_to_service[15]) begin
+            grant_irq = 1'b1;
+            grant_vec = 15;
         end
+    end
+
+        
+        
+    end  // always_comb end
+    
+    // --- Sequential Logic for Register State ---
+    always_ff @(posedge pic_bus.clk or negedge pic_bus.rst_n) begin
+        if (!pic_bus.rst_n) begin
+            enable_int_sav <= 1'b0;
+        end 
+        else if (enable_int_i) begin
+            enable_int_sav <= 1'b1;
+       end
+//       else if (eoi_update_i  && !pending_int) begin
+       // enable_int_sav_clr
+       else if (enable_int_sav_clr) begin
+            enable_int_sav <= 1'b0;
+      end
     end
 
     // --- Sequential Logic for Register State ---
@@ -87,18 +197,19 @@ module pic (
             irr <= 16'h0000;
             imr <= 16'hFFFF; // Safe default: all interrupts are disabled
             isr <= 16'h0000;
+//            pending_int <= 1'b0;  // OR all irr bits
         end else begin
             // Latch any new incoming requests from peripherals
             // A granted request is cleared from IRR in the next section
-//            irr <= irr | pic_bus.irq;                                       // TODO: check this
             irr <= irr | devices_irq;
-//            irr <= irr | irq_i;
             imr <= pic_bus.imr;
-            
+            // Pending interrupt
+//            pending_int <= |pending_and_unmasked;  // Perform the reduction OR operation
+//            pending_int <= |isr;  // Perform the reduction OR operation
             // Clear Interrupt
-            if (eoi_update_i) begin
-                isr[pic_bus.irq_num] <= 1'b0;  // contains the IRQ number to clear
-            end
+//            if (eoi_update_i) begin
+//                isr[pic_bus.irq_num] <= 1'b0;  // contains the IRQ number to clear
+//            end
 
             // Handle the grant of a new interrupt
             if (grant_irq) begin
@@ -107,16 +218,97 @@ module pic (
                 // Clear the corresponding bit from the Request Register, as it's now being serviced
                 irr[grant_vec] <= 1'b0;
             end
+            
+            // clr isr when done with all interrupts
+            if (enable_int_sav_clr) begin
+                isr <= 16'h0000;          // clr
+            end
         end
     end
+ 
+	
+	// =================================
+    // FSM
+    // =================================
+    // State machine
+    typedef enum logic [2:0] {
+        IDLE,
+        SAVE_REGS,
+        WT_EOI,
+        CHK_ISR,
+		NXT_INT
+    } pic_state_t;
+    
+    pic_state_t pic_state;
+    
+    always_ff @(posedge uart_bus.clk or negedge uart_bus.rst_n) begin
+        if (!uart_bus.rst_n) begin
+            pic_state <= IDLE;
+            isr_sav <= '0;
+			grant_irq_gen <= 1'b0;
+			enable_int_sav_clr <= 1'b0;
+			pending_int <= 1'b0;
+        end else begin
+            isr_sav <= '0;  // Default
+			grant_irq_gen <= 1'b0;
+			enable_int_sav_clr <= 1'b0;
+//			pending_int <= 1'b0;
+            
+            case (pic_state)
+                IDLE: begin
+                    if (enable_int_sav && (new_irq)) begin
+                        grant_irq_gen <= 1'b1;
+						pic_state <= SAVE_REGS;
+                    end
+                end
+                
+                SAVE_REGS: begin
+                    // 
+//                    isr_sav       <= isr;
+//					grant_vec_sav <= grant_vec;
+					pending_int <= 1'b1;
+                    pic_state     <= WT_EOI;
+                end
+                
+                // Wait update from control_unit, means end of ISR
+                WT_EOI: begin
+                    isr_sav       <= isr;
+                    if (eoi_update_i) begin
+					    isr[pic_bus.irq_num] <= 1'b0;  // contains the IRQ number to clear
+                        pic_state <= CHK_ISR;
+                    end
+                end
+                               
+                CHK_ISR: begin
+                    if (pending_and_unmasked == 16'h0) begin
+                        enable_int_sav_clr <= 1'b1;
+                        pending_int <= 1'b0;
+                        pic_state <= IDLE;    // done with interrupt, wait for next
+                    end
+					else begin
+					    pic_state <= NXT_INT;
+					end
+                end
+			
+                NXT_INT: begin
+				    isr_sav       <= isr;
+					grant_irq_gen <= 1'b1;
+                    pic_state <= SAVE_REGS;
+                end
+				
+            endcase
+        end
+    end
+    
 
 //================================================================
 // Output assignments to the interface
 //================================================================
-assign pic_bus.irr       = irr;
-assign pic_bus.isr       = isr;
-assign pic_bus.intrpt    = grant_irq;      // granted irq
-assign pic_bus.grant_vec = grant_vec - 1;  // granted irq num, start will zero
+assign pic_bus.irr         = irr;
+assign pic_bus.isr         = isr;
+assign pic_bus.intrpt      = grant_irq;      // granted irq
+assign pic_bus.grant_vec   = grant_vec - 1;  // granted irq num, start will zero
+assign pic_bus.pending_int = pending_int;
 
 
 endmodule
